02389f27a0bd6eecb39732fa5c5397be4d237dae
kwontaeheeee <140040276+kwontaeheeee@users.noreply.github.com>
2025-03-14 12:58:45 +0900

    [level 0] Title: 나이 출력, Time: 0.04 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static int solution(int age) {
+        int answer = 2022 - age + 1;
+        return answer;
+    }
+    public static void main(String args[]){
+        System.out.println(solution(40));
+    }
+}
