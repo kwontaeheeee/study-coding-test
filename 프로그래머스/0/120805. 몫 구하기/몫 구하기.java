6159580e595c4994ff3af9d4339d8889f9cbdb96
kwontaeheeee <140040276+kwontaeheeee@users.noreply.github.com>
2025-03-14 12:47:32 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static int solution(int num1, int num2) {
+        int answer = num1 / num2;
+        return answer;
+    }
+    public static void main(String args[]){
+        System.out.println(solution(10,5));
+    }
+}
