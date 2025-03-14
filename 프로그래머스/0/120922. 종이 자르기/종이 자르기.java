17bcc39c97e1e8555681ac3a5d7501950be47e23
kwontaeheeee <140040276+kwontaeheeee@users.noreply.github.com>
2025-03-14 14:13:56 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int solution(int M, int N) {
+        int answer = M * N - 1;
+        return answer;
+    }
+    public static void main(String args[])
+    {
+        System.out.println(solution(2,2));
+    }
+}
