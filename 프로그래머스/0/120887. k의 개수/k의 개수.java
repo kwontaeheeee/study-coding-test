ca8a9b46849479dd00b8587314b9f4929999fb8e
kwontaeheeee <140040276+kwontaeheeee@users.noreply.github.com>
2025-04-01 12:10:52 +0900

    [level 0] Title: k의 개수, Time: 36.29 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,10 +1,6 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int count = 0;
-        for (int num = i; num <= j; num++) {
-            String str = Integer.toString(num);
-            count += str.length() - str.replace(Integer.toString(k), "").length();
-        }
-        return count;
+        int answer = 0;
+        return answer;
     }
 }
